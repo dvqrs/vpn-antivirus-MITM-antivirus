421813df75e6807d7276dd9d32b80ad801d88191
dvqrs <dvirrx@gmail.com>
2025-04-14 15:51:05 +0300

    client uses tls now

--- clientSideProj.py
+++ clientSideProj.py
@@ -3,21 +3,26 @@ from selenium.webdriver.chrome.options import Options
 import time
 
 
-def create_browser_with_proxy(proxy_address):
+def create_browser_with_tls_proxy(proxy_address):
     chrome_options = Options()
-    # Use HTTP since the TCP proxy is forwarding raw traffic.
-    chrome_options.add_argument(f"--proxy-server=http://{proxy_address}")
+
+    chrome_options.add_argument(f"--proxy-server=https://{proxy_address}")
+
+
+    chrome_options.add_argument("--ignore-certificate-errors")
+
+    chrome_options.set_capability("acceptInsecureCerts", True)
+
     driver = webdriver.Chrome(options=chrome_options)
     return driver
 
 
 if __name__ == "__main__":
-    # Use the external TCP proxy endpoint provided by Railway.
-    proxy = "maglev.proxy.rlwy.net:56449"
-    print("[*] Starting Selenium browser with proxy:", proxy)
 
-    driver = create_browser_with_proxy(proxy)
+    proxy = "shortline.proxy.rlwy.net:22343"
+    print("[*] Starting Selenium browser with TLS proxy at:", proxy)
 
-    # Keep the browser open for testing
+    driver = create_browser_with_tls_proxy(proxy)
+
     time.sleep(1000000)
     driver.quit()
